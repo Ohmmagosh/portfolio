3055ad17efa05778c9ef68b60cd0d035455d6ea5
ohmmaagoch <ohm@42bangkok.com>
2024-06-16 16:30:06 +0000

    Update profile page with website link Add favicon.ico

--- app/app/portfolio/page.tsx
+++ app/app/portfolio/page.tsx
@@ -2,8 +2,10 @@ import Portfolio from "./_components/portfolio";
 
 export default async function PortfolioPage() {
   return (
-    <main className="sm:flex justify-center items-center min-h-screen content-center p-24 bg-slate-900">
-      <Portfolio />
-    </main>
+    <>
+      <main className="sm:flex justify-center items-center min-h-screen content-center p-24 bg-slate-900">
+        <Portfolio />
+      </main>
+    </>
   );
 }
--- app/app/portfolio/resume/_components/header.tsx
+++ app/app/portfolio/resume/_components/header.tsx
@@ -21,19 +21,24 @@ export default function Header(props: THeaderProps) {
     <>
       <header className="flex flex-col lg:flex-row w-full justify-between">
         <div className="text-sm text-left pl-4 md:pl-0">
-          <p className="text-sm">{email}</p>
-          <p className="text-sm">{phone}</p>
-          <p className="text-sm">{location}</p>
+          {[email, phone, location].map((item, index) => {
+            return <p key={index}>{item}</p>;
+          })}
         </div>
         <div className="text-center -order-1 lg:order-none">
-          <h1 className="capitalize text-base md:text-3xl ">
-            {name.toUpperCase()}
-          </h1>
+          <Link href={"/"}>
+            <h1 className="capitalize text-base md:text-3xl ">
+              {name.toUpperCase()}
+            </h1>
+          </Link>
           <h2>({nickname.toUpperCase()})</h2>
         </div>
         <div className="text-left lg:text-right pl-4 md:pl-0">
           <Link href={github}>
             <p className="text-sm hover:underline">{github}</p>
+          </Link>
+          <Link href={website}>
+            <p className="text-sm hover:underline">Ohmmagosh</p>
           </Link>
         </div>
       </header>
